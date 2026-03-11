ada39d76eacc4b03c435c36ce511f7e2d30c911a
Никита Потапов <ns.potapov@yandex.ru>
2026-03-11 21:12:00 +0400

    fix cors

--- backend/src/app/main.py
+++ backend/src/app/main.py
@@ -1,6 +1,7 @@
 import logging
 import uvicorn
 from fastapi import FastAPI, Request, HTTPException, status
+from fastapi.middleware.cors import CORSMiddleware
 
 from api import router
 from app.core.config import settings
@@ -12,6 +13,14 @@ app = FastAPI(
     docs_url="/api/docs",
     openapi_url="/api/openapi.json",
     redoc_url=None,
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 # List of endpoints to block from access logs
